0b1bb7f57ffd7c5a51d75412816d0dc873561b94
abdelrahman <abdelrhamanreda11@gmail.com>
2024-09-21 21:52:32 +0300

    add user authenticated

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import FormLoginStyle from "../../ui/FormLoginStyle";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("abdoreda2060@gmail.com");
+  const [password, setPassword] = useState("12345678");
 
   const { login, isLoading } = useLogin();
   function handleSubmit(e) {
